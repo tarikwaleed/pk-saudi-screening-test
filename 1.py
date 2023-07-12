7d7438dd265a604edec02d5a460371dfb2f99cc2
tarikwaleed <tarikwaleed.tech@gmail.com>
2023-07-12 16:24:43 +0300

    :hammer: edits to Q1

--- 1.py
+++ 1.py
@@ -8,6 +8,9 @@ df = pd.read_csv("input.csv")
 df["BRTHDAT_VALID"] = True
 
 # Loop over the 'birthdate' column
+x=0
+y=0
+report_file = open("BRTHDAT_REPORT.txt", "w")
 for i, cell in enumerate(df["BRTHDAT"]):
     try:
         # Convert the cell value to Hijri date
@@ -16,9 +19,16 @@ for i, cell in enumerate(df["BRTHDAT"]):
         month = int(cell_str[4:6])
         day = int(cell_str[6:8])
         Hijri(year, month, day, validate=True)
-    except (OverflowError, ValueError):
-        # Set 'valid' to False for the corresponding row
+    except OverflowError:
+        x += 1
+        df.loc[i, "BRTHDAT_VALID"] = False
+    except ValueError:
+        y += 1
         df.loc[i, "BRTHDAT_VALID"] = False
 
 # Save the modified DataFrame to output.csv
 df.to_csv("output1.csv", index=False)
+
+report_file.write(f"Gregorian Date: {x} Fields\n")
+report_file.write(f"Invalid Hijri Dates: {y} Fields\n")
+report_file.close()
